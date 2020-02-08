6cdd2f30831b757efb3dd9b2c8f94e19b32e84f1
marc-town <asamin.itconsultant@gmail.com>
2020-02-08 20:30:14 +0900

    ddlにnot null制約追加

--- rdb/initdb.d/002_create_tables.sql
+++ rdb/initdb.d/002_create_tables.sql
@@ -11,20 +11,20 @@ DROP TABLE IF EXISTS `m_tag`;
 create table IF not exists `m_user`
 (
     `id` int not null auto_increment,
-    `login_id` varchar(30),
-    `password` varchar(50),
-    `email` varchar(255),
+    `login_id` varchar(30) not null,
+    `password` varchar(50) not null,
+    `email` varchar(255) not null,
     `name` varchar(255),
-    `create_at` timestamp not null default current_timestamp,
-    `update_at` timestamp not null default current_timestamp on update current_timestamp,
+    `create_at` timestamp default current_timestamp,
+    `update_at` timestamp default current_timestamp on update current_timestamp,
     PRIMARY KEY (`id`)
 ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 create table IF not exists `m_article`
 (
     `id` int not null auto_increment,
-    `title` varchar(255),
-    `body` text,
+    `title` varchar(255) not null,
+    `body` text not null,
     `publish_flag` boolean not null default 0,
     `create_at` timestamp not null default current_timestamp,
     `update_at` timestamp not null default current_timestamp on update current_timestamp,
@@ -35,8 +35,8 @@ create table IF not exists `c_article_tag`
 (
     `article_id` int not null,
     `tag_id` int not null,
-    `create_at` timestamp not null default current_timestamp,
-    `update_at` timestamp not null default current_timestamp on update current_timestamp,
+    `create_at` timestamp default current_timestamp,
+    `update_at` timestamp default current_timestamp on update current_timestamp,
     PRIMARY KEY (`article_id`, `tag_id`)
 ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
@@ -44,7 +44,7 @@ create table IF not exists `m_tag`
 (
     `id` int not null auto_increment,
     `name` varchar(255),
-    `create_at` timestamp not null default current_timestamp,
-    `update_at` timestamp not null default current_timestamp on update current_timestamp,
+    `create_at` timestamp default current_timestamp,
+    `update_at` timestamp default current_timestamp on update current_timestamp,
     PRIMARY KEY (`id`)
 ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
